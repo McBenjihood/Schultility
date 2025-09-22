4c292ea7a69b977850350758505702cbc5614482
McBenjihood <benjaminmikagerresheim@gmail.com>
2025-09-22 23:08:10 +0200

    Edited the vue config to disable filenameHashing and allow injecting a script at the bottom of the index.html file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    filenameHashing: false,
+    chainWebpack: config => {
+        config
+            .plugin('html')
+            .tap(args => {
+                args[0].inject = 'body';
+                args[0].scriptLoading = 'blocking';
+                args[0].scripts = [
+                    ...(args[0].scripts || []),
+                    {
+                        src: 'path/to/your/injector-script.js',
+                    }
+                ];
+                return args;
+            });
+    }
 })
