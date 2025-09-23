ba78efc5079636964685594e116986bf9c38a02f
mcbenjihood <benjaminmikagerresheim@gmail.com>
2025-09-23 16:03:07 +0200

    started working on ui

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,41 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <h1>Title Testing</h1>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="home">
+    <div class="titleElement">
+      <h1>Schultility</h1>
+    </div>
+    <nav class="menu">
+      <router-link to="/" class="routerLinkClass">Home</router-link> |
+      <router-link to="/about" class="routerLinkClass">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
+  .titleElement{
+    padding: 0;
+    margin: 0;
+    text-align: center;
+  }
+  .titleElement h1{
+    padding: 0; margin: 0;
+  }
+  .home{
+    padding: 1em;
+    margin: 0;
+    color: white;
+    font-family: sans-serif;
+    background-color: rgb(12, 12, 22);
+    display: flex;
+    flex-direction: column;
+  }
+  .menu{
+    text-align: center;
+  }
+  .routerLinkClass{
+    color: white;
+  }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,44 @@
+<script setup lang="ts">
+ import {ref} from "vue";
+ let averageGrade = ref(10);
+ let deficitPoints = ref(9);
+ let plusPoints = ref(8);
+ let bestGrade = ref(7);
+ let worstGrade = ref(6000);
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="infoDisplay">
+        <p>Durchschnitt : {{ averageGrade }}</p>
+        <p>Mangelpunkte : {{deficitPoints}}</p>
+        <p>Pluspunkte : {{ plusPoints }}</p>
+        <p>Beste Note : {{ bestGrade }}</p>
+        <p>Schwächste Note : {{ worstGrade}}</p>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+<style scoped>
+.home{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.infoDisplay {
+  font-size: 0.9em;
+  border: darkslategray 2px solid;
+  border-radius: 15px;
+  margin: 0;
+  padding: 10px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 1em;
+  box-sizing: border-box; /* This is the key fix */
+}
+.infoDisplay p{
+  margin: 0;
+  padding: 0;
+  white-space: nowrap;
+}
+</style>
