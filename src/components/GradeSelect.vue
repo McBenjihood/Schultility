23e94a33cf1c7ffa3bc9a767b59b30ec6a4fcb0b
mcbenjihood <benjaminmikagerresheim@gmail.com>
2025-09-26 13:04:28 +0200

    Just syncing current changes to laptop

---
+++ src/components/GradeSelect.vue
@@ -0,0 +1,142 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const profilesArray = ref([
+  {
+    id: 0,
+    text: 'Profile 1',
+  },
+  {
+    id: 1,
+    text: 'Profile 2',},
+  {
+    id: 2,
+    text: 'Profile 3',},
+])
+
+const valueArray = ref([
+  {
+    id: 0,
+    grade: 5.5,
+    subject: 'English'
+  },
+  {
+    id: 1,
+    grade: 4,
+    subject: 'Deutsch'
+  },
+  {
+    id: 2,
+    grade: 5,
+    subject: 'M259',
+    selected: false
+  }
+])
+</script>
+
+<template>
+  <div class="profileSelector">
+    <button v-for="item in profilesArray" :key="item.id" class="selectButton">{{item.text}}</button>
+  </div>
+  <hr>
+  <div class="displaySubjects">
+    <div v-for="item in valueArray" :key="item.id" class="subject">
+      <p>{{item.subject}} :</p>
+      <p>{{item.grade}}</p>
+      <label class="switch">
+        <input type="checkbox" class="switchOutput" v-model="item.subject">
+        <span class="sliderround"></span>
+      </label>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 52px;
+  height: 24px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.sliderround {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  border-radius: 34px;
+}
+
+.sliderround:before {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  border-radius: 50%;
+}
+
+input:checked+.sliderround {
+  background-color: #2196f3;
+}
+
+input:focus+.sliderround {
+  box-shadow: 0 0 1px #2196f3;
+}
+
+input:checked+.sliderround:before {
+  -webkit-transform: translateX(28px);
+  -ms-transform: translateX(28px);
+  transform: translateX(28px);
+}
+.subject{
+  padding: 10px;
+  border: darkslategray 2px solid;
+  border-radius: 15px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 0.1em;
+  align-items: center;
+  margin-bottom: 0.5em;
+}
+hr{
+  border: darkslategray 1px solid;
+}
+.profileSelector {
+  display: flex;
+  justify-content: space-between;
+}
+
+.selectButton {
+  background-color: #2c3e50; /* Dark Blue Gray */
+  border: solid 2px #324F66;
+  color: white;
+  padding: 5px 15px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 12px;
+  transition-duration: 0.4s;
+}
+.selectButton:hover {
+  background-color: #46627f; /* Lighter Blue Gray on hover */
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,46 @@
 <script setup lang="ts">
+import gradeSelector from '/src/components/GradeSelect.vue';
  import {ref} from "vue";
  let averageGrade = ref(10);
  let deficitPoints = ref(9);
  let plusPoints = ref(8);
  let bestGrade = ref(7);
- let worstGrade = ref(6000);
+ let worstGrade = ref(6);
 </script>
 
 <template>
   <div class="home">
     <div class="infoDisplay">
-        <p>Durchschnitt : {{ averageGrade }}</p>
-        <p>Mangelpunkte : {{deficitPoints}}</p>
-        <p>Pluspunkte : {{ plusPoints }}</p>
-        <p>Beste Note : {{ bestGrade }}</p>
-        <p>Schwächste Note : {{ worstGrade}}</p>
+        <p>Durchschnitt :</p> <p>{{ averageGrade }}</p>
+        <p>Mangelpunkte :</p> <p>{{deficitPoints}}</p>
+        <p>Pluspunkte :</p> <p>{{ plusPoints }}</p>
+        <p>Beste Note :</p> <p>{{ bestGrade }}</p>
+        <p>Schwächste Note :</p> <p>{{ worstGrade }}</p>
+    </div>
+    <div class="GradeSelectClass">
+      <grade-selector></grade-selector>
     </div>
   </div>
 </template>
 
 <style scoped>
+.infoDisplay, .GradeSelectClass {
+  font-size: 0.9em;
+  border: darkslategray 2px solid;
+  border-radius: 15px;
+  margin: 0;
+  margin-bottom: 0.5em;
+  padding: 10px;
+}
 .home{
   display: flex;
   flex-direction: column;
   justify-content: center;
 }
 .infoDisplay {
-  font-size: 0.9em;
-  border: darkslategray 2px solid;
-  border-radius: 15px;
-  margin: 0;
-  padding: 10px;
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(4, 1fr);
   column-gap: 1em;
-  box-sizing: border-box; /* This is the key fix */
 }
 .infoDisplay p{
   margin: 0;
