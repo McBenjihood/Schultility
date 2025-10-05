b36de74008205e4f9476a17fbf80064096a9a6d9
mcbenjihood <benjaminmikagerresheim@gmail.com>
2025-10-05 14:09:13 +0200

    Reverted changes to vue.config.js and trying to add script tag manually via template index.html. Also small change to injector so it gets the correct path.

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    filenameHashing: false,
-    chainWebpack: config => {
-        config
-            .plugin('html')
-            .tap(args => {
-                args[0].inject = 'body';
-                args[0].scripts = args[0].scripts || [];
-                args[0].scripts.push({
-                    tagName: 'script',
-                    attributes: {
-                        src: 'js/injector.js'
-                    }
-                });
-                return args;
-            });
-    }
+    filenameHashing: false
 })
