c096d089e5cd1c390a02036126378252e84edc0e
mcbenjihood <benjaminmikagerresheim@gmail.com>
2025-10-05 13:58:40 +0200

    Updated vue config to hopefully inject injector script into index.html

--- vue.config.js
+++ vue.config.js
@@ -7,13 +7,13 @@ module.exports = defineConfig({
             .plugin('html')
             .tap(args => {
                 args[0].inject = 'body';
-                args[0].scriptLoading = 'blocking';
-                args[0].scripts = [
-                    ...(args[0].scripts || []),
-                    {
-                        src: 'path/to/your/injector-script.js',
+                args[0].scripts = args[0].scripts || [];
+                args[0].scripts.push({
+                    tagName: 'script',
+                    attributes: {
+                        src: 'js/injector.js'
                     }
-                ];
+                });
                 return args;
             });
     }
