ae585675fafe7168c1b4736dd6b68087f62b6174
mcbenjihood <benjaminmikagerresheim@gmail.com>
2025-10-05 14:38:20 +0200

    Updating workflow to use html directory from now on and small path change in injector.js

--- public/js/injector.js
+++ public/js/injector.js
@@ -4,6 +4,6 @@ browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
 
   browser.scripting.executeScript({
     target: { tabId: currentTabId },
-    files: ['./injected.js']
+    files: ['/html/js/injected.js']
   });
 });
