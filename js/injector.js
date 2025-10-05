3b7e8718df80665ebf25f95d97f9bafa61940269
mcbenjihood <benjaminmikagerresheim@gmail.com>
2025-10-05 14:45:01 +0200

    Still migrating between file paths.

--- js/injector.js
+++
@@ -1,9 +0,0 @@
-const browser = this.browser || this.chrome;
-browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-  const currentTabId = tabs[0].id;
-
-  browser.scripting.executeScript({
-    target: { tabId: currentTabId },
-    files: ['/js/injected.js']
-  });
-});
