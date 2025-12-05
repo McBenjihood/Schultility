d8cee5c5b45fc8c95883540c3b1b912fad791fc5
McBenjihood <88515315+McBenjihood@users.noreply.github.com>
2025-12-05 12:04:42 +0000

    build(vue): Update built files in root from development build

---
+++ js/injector.js
@@ -0,0 +1,8 @@
+const browser = this.browser || this.chrome;
+browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  const currentTabId = tabs[0].id;
+  browser.scripting.executeScript({
+    target: { tabId: currentTabId },
+    files: ['dist/js/injected.js']
+  });
+});
