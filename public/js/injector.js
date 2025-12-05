90ef7b25e1207502ad10d08bb56dcbe13736e34b
mcbenjihood <benjaminmikagerresheim@gmail.com>
2025-12-05 12:59:18 +0100

    Testing new workflow

--- public/js/injector.js
+++ public/js/injector.js
@@ -1,7 +1,6 @@
 const browser = this.browser || this.chrome;
 browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
   const currentTabId = tabs[0].id;
-
   browser.scripting.executeScript({
     target: { tabId: currentTabId },
     files: ['html/js/injected.js']
